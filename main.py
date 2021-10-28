555055ed101ad595e42a2989a0cdb9940a9cc4ac
Nikolas Mikolaski <mikolaskin@gmail.com>
2021-10-28 15:17:44 -0400

    -cleared up extra code that was not needed after fixes.

--- main.py
+++ main.py
@@ -12,14 +12,12 @@ OUTPUT_IMAGE_LIBRARY = "images/output/"
 
 
 def main():
-    prepare_collage_images(200)
+    block_size = 200
+    prepare_collage_images(block_size)
     im = Image.open("base.png")
-    # Use a breakpoint in the code line below to debug your script.
-    # print('Testing Begin')  # Press Ctrl+F8 to toggle the breakpoint.
     im_array = load_image_into_array(im)
     im_width, im_height = im.size
-    calculate_block_average_color(im_array, im_width, im_height)
-    # test = find_closes_matching_image_to_color([11, 100 , 90], SOURCE_CROPPED_LIBRARY)
+    calculate_block_average_color(im_array, im_width, im_height, block_size)
 
 
 def load_image():
@@ -67,9 +65,6 @@ def load_image_into_array(image):
     # load image into array
     image_array = numpy.array(image)
     # image_array = numpy.asarray(image)
-    # image_array = list(image.getdata())
-    # im_array = list(image.getdata())
-    # return [im_array[i:i + image.width] for i in range(0, len(im_array), image.width)]
     return image_array
 
 
@@ -135,11 +130,7 @@ def find_average_color(image_array):
     g_total = 0
     b_total = 0
     pixel_number_total = 0
-    # for x in range(0, len(image_array) - 1):
-    #    for y in range(0, len(image_array) - 1):
-    #        red_list.append(image_array[x][y][0])
-    #        green_list.append(image_array[x][y][1])
-    #        blue_list.append(image_array[x][y][2])
+
     for row in image_array:
         for color_tuple in row:
             r_total += color_tuple[0]
@@ -147,18 +138,10 @@ def find_average_color(image_array):
             b_total += color_tuple[2]
             pixel_number_total += 1
 
-    # r = int(round(sum(red_list) / len(red_list)))
-    # g = int(round(sum(green_list) / len(green_list)))
-    # b = int(round(sum(blue_list) / len(blue_list)))
     r_average = int(round(r_total / pixel_number_total))
     g_average = int(round(g_total / pixel_number_total))
     b_average = int(round(b_total / pixel_number_total))
 
-    # print(r)
-    # print(g)
-    # print(b)
-
-    # return r, g, b
     return r_average, g_average, b_average
 
 
@@ -167,35 +150,26 @@ def color_block(color, new_image, x, y, block_width, block_height):
     draw.rectangle((x, y, block_width, block_height), fill=(color[0], color[1], color[2]))
 
 
-def calculate_block_average_color(image_array, image_width, image_height):
+def calculate_block_average_color(image_array, image_width, image_height, block_size):
     # for the size of the image, iterate and grab every tuple into two dimensional array
     output_image = Image.new('RGB', (image_width, image_height), (255, 255, 255, 0))
-    block_array = []
-    block_width = 200
-    block_height = 200
-    total = 0
+
     # divide by floor length to put into array and add them all
     draw = ImageDraw.Draw(output_image)
-
-    for row in range(0, image_width - 1, block_width):
+    for row in range(0, image_width - 1, block_size):
         # divide by floor height to put into right array to add them all
-        for col in range(0, image_height - 1, block_height):
-            # print("Row #" + str(row))
-            # print("Col #" + str(col))
-            block = get_image_block(image_array, (col, row), block_width)
-            # block = get_image_square(image_array, (row, col), block_width)
+        for col in range(0, image_height - 1, block_size):
+            block = get_image_block(image_array, (col, row), block_size)
             average_color = find_average_color(block)
-            # average_color = mean_rgb(block)
-            # draw needs to be x0,y0, x0+width, y0+ height
 
             matching_image = find_closes_matching_image_to_color(average_color, SOURCE_CROPPED_LIBRARY)
             block_image = Image.open(SOURCE_CROPPED_LIBRARY + matching_image)
             output_image.paste(block_image, (row, col))
+
+            # draw needs to be x0,y0, x0+width, y0+ height
             # draw.rectangle((row, col, row + block_width, col + block_height), fill=(average_color[0], average_color[1], average_color[2]))
             output_image.show()
         # print(image_array[row][col])
-
-        # add the amount to total
 
     # for the amount of blocks in the image, calculate the average R,G,B of the block size
     output_image.show()
